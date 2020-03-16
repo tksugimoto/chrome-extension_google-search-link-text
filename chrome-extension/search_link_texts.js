f46124a790adb2fdf34e3e69e0fc32fe1a7ac677
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2020-03-17 00:41:54 +0900

    refactor: fix: Mixed spaces and tabs.

--- chrome-extension/search_link_texts.js
+++ chrome-extension/search_link_texts.js
@@ -24,9 +24,7 @@
 	};
 	chrome.runtime.onMessage.addListener(request => {
 		if (request.method === 'searchLinkTexts') {
-			if (latestContextMenuTriggeredAnchorElement
-			 && latestContextMenuTriggeredAnchorElement.href === request.linkUrl
-			) {
+			if (latestContextMenuTriggeredAnchorElement && latestContextMenuTriggeredAnchorElement.href === request.linkUrl) {
 				const linkText = latestContextMenuTriggeredAnchorElement.innerText.trim();
 				if (linkText) {
 					chrome.runtime.sendMessage({
